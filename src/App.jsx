d35519d6c3426ae6e2c8dee4d7e8f7bf35337dc9
Ogi Dimitrijevic <ogi@ogidesigns.ca>
2024-06-18 09:56:32 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './componenets/Header/Header'
+import Warehouses from './pages/Warehouses/Warehouses'
+import Inventory from './pages/Inventory/Inventory'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Warehouses />} />
+      <Route path="/warehouses" element={<Warehouses />} />
+      <Route path="/inventory" element={<Inventory />} />
+    </Routes>
+  </Router>
   )
 }
 
---
+++ src/componenets/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import logo from '../../assets/Logo/InStock-Logo.svg'
+import './Header.scss'
+import Button from '../Button/Button';
+
+// created routes for warehouse and inventory, 
+// not sure if these will be final routes so not adding them to app.
+// created button component so we can reuse it later 
+// created onClick functions that use useNavigate to route to respective pages
+//button takes active as a prop which is equal to the pathname that we are setting using useLocation
+
+export default function Header() {
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    function warehouseClick() {
+        navigate('/warehouses');
+    };
+    
+    function inventoryClick() {
+        navigate('/inventory'); 
+      };
+    return (
+        <header className='header'>
+            <div className='header__logo-container'>
+            <img className='header__logo' src={logo} alt='InStock logo' />
+            </div>
+            <nav className='header__nav'>
+                <Button text='Warehouses' onClick={warehouseClick} active={location.pathname === '/warehouses'} />
+                <Button text='Inventory' onClick={inventoryClick} active={location.pathname === '/inventory'} />
+            </nav>
+        </header>
+    )
+}
