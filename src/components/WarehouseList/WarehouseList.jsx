229f84536fcb9cef024206f9b4944088e68f4f57
Eo Soo Kim <eosookim@gmail.com>
2024-06-18 16:25:38 -0400

    makes api call to get warehouse data, removes hardcoded data

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,47 +1,33 @@
 import "./WarehouseList.scss";
-import { useState, setState } from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 import SingleWarehouse from "../SingleWarehouse/SingleWarehouse";
 import sortIcon from "../../assets/Icons/sort-24px.svg";
 
 function WarehouseList() {
   const [warehouses, setWarehouses] = useState([
     {
-      warehouse: "Manhatten",
-      address: "503 Broadway, New York, USA",
-      contactName: "Parmin Aujla",
-      contactInfo: "+1 (629) 555-0129 paujla@instock.com",
-    },
-    {
-      warehouse: "Washington",
-      address: "33 Pearl Street SW, Washington, USA",
-      contactName: "Graeme Lyon",
-      contactInfo: "+1 (647) 504-0911 glyon@instock.com",
-    },
-    {
-      warehouse: "Jersey",
-      address: "300 Main Street, New Jersey, USA",
-      contactName: "Brad MacDonald",
-      contactInfo: "+1 (401) 377-2337 bmcdonald@instock.com",
-    },
-    {
-      warehouse: "Manhatten",
-      address: "503 Broadway, New York, USA",
-      contactName: "Parmin Aujla",
-      contactInfo: "+1 (629) 555-0129 paujla@instock.com",
-    },
-    {
-      warehouse: "Washington",
-      address: "33 Pearl Street SW, Washington, USA",
-      contactName: "Graeme Lyon",
-      contactInfo: "+1 (647) 504-0911 glyon@instock.com",
-    },
-    {
-      warehouse: "Jersey",
-      address: "300 Main Street, New Jersey, USA",
-      contactName: "Brad MacDonald",
-      contactInfo: "+1 (401) 377-2337 bmcdonald@instock.com",
-    },
+        id: 1,
+        warehouse_name: "",
+        address: "",
+        city: "",
+        country: "",
+        contact_name: "",
+        contact_position: "",
+        contact_phone: "",
+        contact_email: "",
+      }
   ]);
+
+  useEffect(() => {
+    const getWarehouseList = async () => {
+        const response = await axios.get(
+            `http://localhost:8080/`
+        )
+        setWarehouses(response.data);
+    }
+    getWarehouseList();
+  })
   return (
     <div className="warehouse-list">
       <div className="warehouse-list__header">
@@ -83,10 +69,10 @@ function WarehouseList() {
           return (
             <SingleWarehouse
               key={index}
-              warehouse={warehouse.warehouse}
-              address={warehouse.address}
-              contactName={warehouse.contactName}
-              contactInfo={warehouse.contactInfo}
+              warehouse={warehouse.warehouse_name}
+              address={warehouse.address + ', ' + warehouse.city + ', ' + warehouse.country}
+              contactName={warehouse.contact_name}
+              contactInfo={warehouse.contact_phone + " " + warehouse.contact_email}
             />
           );
         })}
