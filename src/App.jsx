d2169362bb9dc90b805c520fd59e6ed69ad49b61
Bao <bao9600455@yahoo.com.vn>
2024-06-18 14:58:58 -0400

    inventory list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WareHouse from "./pages/WareHouse/WareHouse";
-import Inventory from "./pages/Inventory/Inventory";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import Header from "./componenets/Header/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WareHouse from './pages/WareHouse/WareHouse'
+import Inventory from './pages/Inventory/Inventory'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Header from './components/Header/Header'
 import './App.scss'
 function App() {
   return (
@@ -10,14 +10,14 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<WareHouse />}></Route>
-          <Route path="/warehouse/:id" element={<WareHouse />}></Route>
-          <Route path="/inventory" element={<Inventory />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
+          <Route path='/' element={<WareHouse />}></Route>
+          <Route path='/warehouse/:id' element={<WareHouse />}></Route>
+          <Route path='/inventory' element={<Inventory />}></Route>
+          <Route path='*' element={<ErrorPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,93 +1,93 @@
-import "./Category.scss";
-import sort from "../../assets/images/sort-24px.svg";
-import { Link } from "react-router-dom";
-import chevron from "../../assets/images/chevron_right-24px.svg";
-import del from "../../assets/images/delete_outline-24px.svg";
-import edit from "../../assets/images/edit-24px.svg";
-import ListItem from "../ListItem/ListItem";
+import './Category.scss'
+import sort from '../../assets/images/sort-24px.svg'
+import { Link } from 'react-router-dom'
+import chevron from '../../assets/images/chevron_right-24px.svg'
+import del from '../../assets/images/delete_outline-24px.svg'
+import edit from '../../assets/images/edit-24px.svg'
+import ListItem from '../ListItem/ListItem'
 const Category = () => {
   return (
     <>
-      <div className="category">
-        <div className="category-containers">
-          <div className="category-container">
-            <div className="category-item">
-              <h6 className="category-item-text">IVENTORY ITEM</h6>
+      <div className='category'>
+        <div className='category-containers'>
+          <div className='category-container'>
+            <div className='category-item'>
+              <h6 className='category-item-text'>IVENTORY ITEM</h6>
               <img
-                className="category-item-img category-sort-one"
+                className='category-item-img category-sort-one'
                 src={sort}
                 alt={`${sort}logo`}
               />
-              <div className="category-item-link">
-                <Link to="/">
-                  <p className="category-item-link-text category-mobile">
+              <div className='category-item-links'>
+                <Link className='category-item-link' to='/'>
+                  <p className='category-item-link-text category-mobile'>
                     Television
                   </p>
                   <img
-                    className="category-item-link-chev category-mobile"
+                    className='category-item-link-chev category-mobile'
                     src={chevron}
                     alt={`${chevron} logo`}
                   />
                 </Link>
               </div>
-              <div className="category-item-container">
-                <h6 className="category-item-container--text">CATEGORY</h6>
+              <div className='category-item-container'>
+                <h6 className='category-item-container--text'>CATEGORY</h6>
                 <img
-                  className="category-item-img category-sort-two"
+                  className='category-item-img category-sort-two'
                   src={sort}
                   alt={`${sort}logo`}
                 />
-                <p className="category-item-cat category-mobile">Electronics</p>
+                <p className='category-item-cat category-mobile'>Electronics</p>
               </div>
-              <div className="category-logo">
+              <div className='category-logo'>
                 <img
-                  className="category-logo-del"
+                  className='category-logo-del'
                   src={del}
                   alt={`${del} logo `}
                 />
                 <img
-                  className="category-logo-edit"
+                  className='category-logo-edit'
                   src={edit}
                   alt={`${edit} logo `}
                 />
               </div>
             </div>
-            <div className="category-container-one">
-              <div className="category-status">
-                <h6 className="category-status-text">STATUS</h6>
+            <div className='category-container-one'>
+              <div className='category-status'>
+                <h6 className='category-status-text'>STATUS</h6>
                 <img
-                  className="category-item-img category-sort-three"
+                  className='category-item-img category-sort-three'
                   src={sort}
                   alt={`${sort}logo`}
                 />
-                <p className="category-item-stock category-mobile">In Stock</p>
+                <p className='category-item-stock category-mobile'>In Stock</p>
               </div>
-              <div className="category-quantity">
-                <h6 className="category-quantity-text">QTY</h6>
+              <div className='category-quantity'>
+                <h6 className='category-quantity-text'>QTY</h6>
                 <img
-                  className="category-item-img category-sort-four"
+                  className='category-item-img category-sort-four'
                   src={sort}
                   alt={`${sort}logo`}
                 />
-                <p className="category-item-cat category-mobile">500</p>
+                <p className='category-item-cat category-mobile'>500</p>
               </div>
-              <div className="category-warehouse">
-                <h6 className="category-warehouse-text">WAREHOUSE</h6>
+              <div className='category-warehouse'>
+                <h6 className='category-warehouse-text'>WAREHOUSE</h6>
                 <img
-                  className="category-item-img category-sort-five"
+                  className='category-item-img category-sort-five'
                   src={sort}
                   alt={`${sort}logo`}
                 />
-                <p className="category-item-cat category-mobile">Manhattan</p>
+                <p className='category-item-cat category-mobile'>Manhattan</p>
               </div>
-              <h6 className="category-warehouse-action">ACTIONS</h6>
+              <h6 className='category-warehouse-action'>ACTIONS</h6>
             </div>
           </div>
           <ListItem />
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Category;
+export default Category
