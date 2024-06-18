ce70789bc94d3ccdf8e262e0221fdffe68891cd9
Jenna Knechtel <jmknec@gmail.com>
2024-06-18 10:50:04 -0400

    create add warehouse component files

---
+++ src/components/AddWarehouse/AddWarehouse.jsx
@@ -0,0 +1,9 @@
+import "./AddWarehouse.scss";
+
+function AddWarehouse() {
+  return (
+    <div className="add-warehouse">
+      <h1 className="add-warehouse__title">Add New Warehouse</h1>
+    </div>
+  );
+}
