6576863ee3b50386c9ba6c12cc2b699ccdcdabf5
Bao <bao9600455@yahoo.com.vn>
2024-06-20 13:16:13 -0400

    mobile responsive and dynamic

--- src/components/InventoryDetails/InventoryDetails.jsx
+++ src/components/InventoryDetails/InventoryDetails.jsx
@@ -2,28 +2,35 @@ import "./InventoryDetails.scss";
 import arrowBack from "../../assets/images/arrow_back-24px.svg";
 import edit from "../../assets/images/edit-24px.svg";
 import InventoryDetailsList from "../InventoryDetailsList/InventoryDetailsList";
-const InventoryDetails = () => {
+import { Link } from "react-router-dom";
+const InventoryDetails = ({ itemDataDetails, warehouseDetails }) => {
+  const { item_name } = itemDataDetails;
   return (
     <nav className="inventories">
       <section className="inventories-container">
         <div className="inventories-container-header">
-          <img
-            className="inventories-container-arrow"
-            src={arrowBack}
-            alt={`{arrowBack} logo`}
-          />
-          <h1 className="inventories-container-title">Television</h1>
+          <Link to="/inventory">
+            <img
+              className="inventories-container-arrow"
+              src={arrowBack}
+              alt="Back arrow logo"
+            />
+          </Link>
+          <h1 className="inventories-container-title">{item_name}</h1>
         </div>
         <div className="inventories-container-edit">
           <img
             className="inventories-container-edit-img"
             src={edit}
-            alt={`{edit} logo}`}
+            alt="Edit logo"
           />
           <h3 className="inventories-container-edit-text">Edit</h3>
         </div>
       </section>
-      <InventoryDetailsList />
+      <InventoryDetailsList
+        itemDataDetails={itemDataDetails}
+        warehouseDetails={warehouseDetails}
+      />
     </nav>
   );
 };
--- src/components/InventoryDetailsList/InventoryDetailsList.jsx
+++ src/components/InventoryDetailsList/InventoryDetailsList.jsx
@@ -1,29 +1,43 @@
 import "./InventoryDetailsList.scss";
-const InventoryDetailsList = () => {
+const InventoryDetailsList = ({ itemDataDetails, warehouseDetails }) => {
+  const { description, category, status, quantity } = itemDataDetails;
+  const warehouseName = warehouseDetails.find(
+    (warehouse) => warehouse.id === itemDataDetails.id
+  );
   return (
     <div className="item">
       <div className="item-container">
         <div className="item-container-one">
           <h6 className="item-container-one-title">ITEM DESCRIPTION:</h6>
-          <p className="item-container-one-description">asdasdasd</p>
+          <p className="item-container-one-description">{description}</p>
         </div>
         <div className="item-container-two">
           <h6 className="item-container-two-category">CATEGORY</h6>
-          <p className="item-container-two-item">Electronics</p>
+          <p className="item-container-two-item">{category}</p>
         </div>
       </div>
       <div className="item-container-right">
         <div className="item-container-right-one">
           <h6 className="item-container-right-one-status">STATUS:</h6>
-          <p className="item-container-right-one-stock">INSTOCK</p>
+          <p
+            className={`item-container-right-one-stock ${
+              status === "Out of Stock"
+                ? "item-container-right-one-notstock"
+                : ""
+            }`}
+          >
+            {status}
+          </p>{" "}
         </div>
         <div className="item-container-right-two">
           <h6 className="item-container-right-two-warehouse">WAREHOUSE</h6>
-          <p className="item-container-right-two-location">Washington</p>
+          <p className="item-container-right-two-location">
+            {warehouseName.warehouse_name}
+          </p>
         </div>
         <div className="item-container-right-three">
           <h6 className="item-container-right-three-quantity">QUANTITY:</h6>
-          <p className="item-container-right-three-quans">500</p>
+          <p className="item-container-right-three-quans">{quantity}</p>
         </div>
       </div>
     </div>
