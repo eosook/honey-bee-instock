bb9161732a644b0e6fbdfbafc7cdf23f7caf56e6
Eo Soo Kim <eosookim@gmail.com>
2024-06-22 00:49:01 -0400

    remakes the listItem component, adds responsive styling

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -16,74 +16,77 @@ const ListItem = ({
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
 
   return (
-    <>
-          <div className="list" key={item.id}>
-            <div className="list-containers">
-              <div className="list-container">
-                <Link
-                  className="list-container-link"
-                  to={`/inventory/${item.id}`}
-                >
-                  <p className="list-container-item">{item.item_name}</p>
-                  <img
-                    className="list-container-chev"
-                    src={chevron}
-                    alt="Chevron logo"
-                  />
-                </Link>
-              </div>
-              <div className="list-item-one">
-                <p className="list-item-category">{item.category}</p>
-              </div>
-              <div className="list-item-two">
-                <p
-                  className={`list-item-status ${
-                    item.status === "Out of Stock"
-                      ? "list-item-status-notstock"
-                      : ""
-                  }`}
-                >
-                  {item.status}
-                </p>
-              </div>
-              <div
-                className={` list-item-three ${
-                  isWarehouse ? "list-item-quantity-warehouse" : ""
-                }`}
-              >
-                <p className="list-item-quantity">{item.quantity}</p>
-              </div>
-              <div
-                className={`list-item-four ${
-                  isWarehouse ? "list-item-remove" : ""
-                }`}
-              >
-                <p className="list-item-location">
-                  {warehouseName.warehouse_name}
-                </p>
-              </div>
-              <div className="list-logo">
-                <a href="#top">
-                  <img
-                    onClick={() => setOpenDeleteModal(true)}
-                    className="list-logo-del"
-                    src={del}
-                    alt="Delete logo"
-                  />
-                </a>
-                <DeleteInventoryModal
-                  itemData={itemData}
-                  setItemData={setItemData}
-                  id={item.id}
-                  item={item.item_name}
-                  open={openDeleteModal}
-                  onClose={() => setOpenDeleteModal(false)}
-                />
-                <img className="list-logo-edit" src={edit} alt="Edit logo" />
-              </div>
-            </div>
+    <div className="inventory-item">
+      <div className="inventory-item__data-container">
+        <div className="inventory-item__column">
+          <div className="inventory-item__name">
+            <h3 className="inventory-item__label">INVENTORY ITEM</h3>
+            <Link
+              className="inventory-item__link inventory-item__body"
+              to={`/inventory/${item.id}`}
+            >
+              <p>{item.item_name}</p>
+              <img
+                className="inventory-item__chevron"
+                src={chevron}
+                alt="Chevron logo"
+              />
+            </Link>
           </div>
-    </>
+          <div className="inventory-item__category">
+            <h3 className="inventory-item__label">CATEGORY</h3>
+            <p className="inventory-item__body">{item.category}</p>
+          </div>
+        </div>
+        <div className="inventory-item__column">
+          <div className="inventory-item__status">
+            <h3 className="inventory-item__label">STATUS</h3>
+            <p
+              className={`inventory-item__body inventory-item-status ${
+                item.status === "Out of Stock"
+                  ? "inventory-item-status-notstock"
+                  : ""
+              }`}
+            >
+              {item.status}
+            </p>
+          </div>
+          <div className={`inventory-item__quantity ${
+              isWarehouse ? "inventory-item__quantity-warehouse" : ""
+            }`}>
+            <h3 className="inventory-item__label">QTY</h3>
+            <p className="inventory-item__body">{item.quantity}</p>
+          </div>
+          <div className={`inventory-item__warehouse ${
+              isWarehouse ? "inventory-item__remove" : ""
+            }`}>
+            <h3 className="inventory-item__label">WAREHOUSE</h3>
+            <p className="inventory-item__body">
+              {warehouseName.warehouse_name}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="inventory-item__logo">
+        <a href="#top">
+          <img
+            onClick={() => setOpenDeleteModal(true)}
+            className="inventory-item__del"
+            src={del}
+            alt="Delete logo"
+          />
+        </a>
+        <DeleteInventoryModal
+          itemData={itemData}
+          setItemData={setItemData}
+          id={item.id}
+          item={item.item_name}
+          open={openDeleteModal}
+          onClose={() => setOpenDeleteModal(false)}
+        />
+        <img className="inventory-item__edit" src={edit} alt="Edit logo" />
+      </div>
+    </div>
   );
 };
 
